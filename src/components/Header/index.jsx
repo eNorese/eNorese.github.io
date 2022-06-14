bee977b97741e09d3d546ea84fe75f481a55bbfe
Enzo Norese <enzo.norese@gmail.com>
2022-06-14 03:34:00 -0400

    Add: Navigation bar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,38 @@
-import './Header.css'
+import { useState } from 'react';
+import './Header.css';
+import logo from './../../assets/enorese-logo.png';
 
 function Header() {
+    const [iconMenu, setIconMenu] = useState('icon-menu');
+    const [navVisibility, setNavVisibility] = useState('nav-hidden');
+
+    const MenuHandler = () => {
+        if (iconMenu === 'icon-menu') {
+            setIconMenu('icon-cancel');
+            setNavVisibility('nav-visible');
+        } else {
+            setIconMenu('icon-menu');
+            setNavVisibility('nav-hidden');
+        }
+    }
+
     return (
         <header>
             <div className='logo'>
-                <img src='../../img/enorese-logo.png' alt='enorese.dev' />
+                <img src={logo} alt='enorese.dev' />
             </div>
-            <nav className='navigation nav-hidden'>
+
+            <nav className={`navigation ${navVisibility}`}>
                 <ul>
                     <li>Sobre Mí</li>
                     <li>Proyectos</li>
                     <li>Contacto</li>
                 </ul>
             </nav>
-            <button className='menu-btn'><span className='icon-menu'></span></button>
+            
+            <button className='menu-btn' onClick={MenuHandler}>
+                <span className={iconMenu}></span>
+            </button>
         </header>
     );
 }
